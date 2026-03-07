f470915db6f95673972144dc8ee27e0fd4900e6f
paulosgio <paulosergiodosanjossilvafilho@gmail.com>
2026-03-07 16:29:16 -0300

    itersection

---
+++ intersectionofTwoArrays/index.js
@@ -0,0 +1,19 @@
+function intersection(nums1, nums2) {
+    let arr = [];
+    let obj = {};
+    for (let i = 0; i < nums1.length; i++) {
+        for (let j = 0; j < nums2.length; j++) {
+            if (nums1[i] === nums2[j]) {
+                if (arr.includes(nums1[i])) {
+                    break;
+                }
+                else {
+                    arr.push(nums1[i]);
+                }
+            }
+        }
+    }
+    return arr;
+}
+;
+console.log(intersection([4, 9, 5], [9, 4, 9, 8, 4]));
---
+++ intersectionofTwoArrays/index.ts
@@ -0,0 +1,16 @@
+function intersection(nums1: number[], nums2: number[]): number[] {
+    let arr: number[] = []
+    for (let i = 0; i < nums1.length; i++) {
+        for (let j = 0; j < nums2.length; j++) {
+            if (nums1[i] === nums2[j]) {
+                if (arr.includes(nums1[i])) {
+                    break
+                } else {
+                    arr.push(nums1[i])
+                }
+            }
+        }        
+    }
+    return arr
+};
+console.log(intersection([4,9,5], [9,4,9,8,4]))
