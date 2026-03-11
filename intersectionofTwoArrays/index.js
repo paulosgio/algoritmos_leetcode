4d19f2649e0c1d222642b389c6de68152e786bb9
paulosgio <paulosergiodosanjossilvafilho@gmail.com>
2026-03-11 17:19:29 -0300

    n sei

--- intersectionofTwoArrays/index.js
+++ intersectionofTwoArrays/index.js
@@ -1,6 +1,5 @@
 function intersection(nums1, nums2) {
     let arr = [];
-    let obj = {};
     for (let i = 0; i < nums1.length; i++) {
         for (let j = 0; j < nums2.length; j++) {
             if (nums1[i] === nums2[j]) {
