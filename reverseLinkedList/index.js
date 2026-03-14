24bc41228bd4ac789cb32f3fec5cd47ded9f8df9
paulosgio <paulosergiodosanjossilvafilho@gmail.com>
2026-03-14 19:59:21 -0300

    idk

--- reverseLinkedList/index.js
+++ reverseLinkedList/index.js
@@ -1,22 +1,23 @@
-class ListNode {
-    constructor(value, next = null) {
-        this.value = value;
-        this.next = next;
+/* class ListNode {
+    value: number
+    next: ListNode | null
+    constructor(value: number, next: ListNode | null = null) {
+        this.value = value
+        this.next = next
     }
-}
-function reverseList(head) {
-    let newList = null;
+} */
+/* function reverseList(head: ListNode | null): ListNode | null {
+    let newList = null
     while (head) {
-        let nextNode = head.next;
-        head.next = newList;
-        newList = head;
-        head = nextNode;
+        let nextNode = head.next
+        head.next = newList
+        newList = head
+        head = nextNode
     }
-    return newList;
-}
-;
-const node3 = new ListNode(3);
-const node2 = new ListNode(2, node3);
-const node1 = new ListNode(1, node2);
-const head = node1;
-console.log(reverseList(head));
+    return newList
+};
+const node3 = new ListNode(3)
+const node2 = new ListNode(2,node3)
+const node1 = new ListNode(1, node2)
+const head = node1
+console.log(reverseList(head)); */
--- reverseLinkedList/index.ts
+++ reverseLinkedList/index.ts
@@ -1,13 +1,13 @@
-class ListNode {
+/* class ListNode {
     value: number
     next: ListNode | null
     constructor(value: number, next: ListNode | null = null) {
         this.value = value
         this.next = next
     }
-}
+} */
 
-function reverseList(head: ListNode | null): ListNode | null {
+/* function reverseList(head: ListNode | null): ListNode | null {
     let newList = null
     while (head) {
         let nextNode = head.next 
@@ -21,4 +21,4 @@ const node3 = new ListNode(3)
 const node2 = new ListNode(2,node3)
 const node1 = new ListNode(1, node2)
 const head = node1
-console.log(reverseList(head));
+console.log(reverseList(head)); */
