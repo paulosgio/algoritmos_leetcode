eba565725b2b590347c863d7a43332a936ce608d
paulosgio <paulosergiodosanjossilvafilho@gmail.com>
2026-03-24 16:56:29 -0300

    bubbleSort

---
+++ bubbleSort/index.ts
@@ -0,0 +1,13 @@
+function BubbleSort(arr: number[]) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] > arr[j+1]) {
+                let n = arr[j]
+                arr[j] = arr[j + 1];
+                arr[j + 1] = n;
+            }
+        }
+    }
+    return arr
+}
+console.log(BubbleSort([5,2,5,3,1]))
